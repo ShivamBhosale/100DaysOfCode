c66c521fa1a10598b69df7d770ec2d7a3c9110aa
Shivam Bhosale <50315818+ShivamBhosale@users.noreply.github.com>
2020-12-22 17:27:39 +0530

    Day_4
    Hangman game

--- Day3/Insertionsort.py
+++ Day3/Insertionsort.py
@@ -2,7 +2,7 @@
 in your hands. 
 The array is virtually split into a sorted and an unsorted part.
 Values from the unsorted part are picked and placed at the correct position in the sorted part.
-- Source Geek fo Geeks"""
+-Source Geek fo Geeks"""
 def insertionSort(arr):
     for i in range(1,len(arr)):
         
---
+++ Day4/hangman.py
@@ -0,0 +1,60 @@
+import time
+import random
+words =["car","window","aligator","bottle","teaandtech","gamingchair"]
+
+x=random.choice(words)
+
+guesses = ''
+
+name=input("Enter your name to begin the game: ")
+
+time.sleep(0.5)
+print("Hello",name,"and welcome to the game of hangman")
+
+level=input("Select you difficulty level - easy, medium, hard: " )
+
+maximum = 0
+if level == 'hard' or level == 'Hard' or level == 'h' or level == "H":
+    maximum=5
+    print(maximum)
+
+elif level == 'medium' or level == 'Medium' or level == 'm' or level == 'M':
+    maximum=8
+
+elif level == 'easy' or level == 'Easy' or level == 'e' or level == 'E':
+    maximum=10
+
+while maximum > 0:
+
+    failed = 0
+
+    for char in x:
+        if char in guesses:
+            print (char)
+
+        else:
+
+            print("_")
+            failed += 1
+
+    if failed == 0:
+        print("You Won")
+        break
+
+    guess= input("Guess a character: ")
+
+    guesses += guess
+
+    if guess not in x:
+            
+        maximum -= 1
+
+        print("Wrong, try another letter")
+
+        if(maximum>1):
+            print("You have",maximum,"turns left")
+        elif(maximum==1):
+            print("You have only",maximum,"turn left")
+            
+        elif maximum == 0:
+            print("You lose")

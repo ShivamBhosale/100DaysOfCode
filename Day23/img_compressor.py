a87b45fa99b241e30dab8d161fba08e620e3ae83
Shivam Bhosale <50315818+ShivamBhosale@users.noreply.github.com>
2021-01-10 18:59:39 +0530

    Day 23
    Image Compressor hosted using streamlit

---
+++ Day23/img_compressor.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from PIL import Image 
+
+
+st.write(""" # Image Compressor """)
+st.write(""" ## Made by: Shivam Bhosale """)
+uploaded_file = st.file_uploader("Upload your Image to be compressed here, As of now it only accepts jpg images and reduces their dimension thereby compressing the image", type=".jpg")
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image',width=300)
+    st.write("The image is compressed")
+    
+    if st.button('Download'):
+        st.write("To Download the Image right-click on the image and select 'Save Image As' ")

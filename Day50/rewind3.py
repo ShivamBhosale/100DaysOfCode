2cbba20e0fbe7597aa3c5db4e1e4f2c03b9e9cc4
Shivam Bhosale <50315818+ShivamBhosale@users.noreply.github.com>
2021-02-06 21:30:55 +0530

    Update

--- Day50/rewind3.py
+++ Day50/rewind3.py
@@ -3,7 +3,7 @@ import pyfiglet
 
 ascii_banner = pyfiglet.figlet_format("Rewind 3")
 print(ascii_banner)
-"""
+
 def bubblesort(arr):
     swapped = True
 
@@ -48,7 +48,7 @@ def LinearSearch(arr,ele):
             return False
 
 print(LinearSearch([10,20,30],20))
-"""
+
 
 def BinarySearch(arr,ele):
     first = 0
@@ -68,4 +68,82 @@ def BinarySearch(arr,ele):
 
 print("The number is found at array index: ",BinarySearch([10,20,30,40,50],40))
         
-        
+
+class Person:
+    amount = 0
+    def __init__(self,name,age,weight):
+        self.name = name
+        self.age = age
+        self.weight = weight
+        Person.amount += 1
+
+    def __str__(self):
+        return (f"Name: {self.name}, Age: {self.age}, Weight {self.weight}")
+
+
+x =  Person("Shivam",21,64)
+print(x)
+
+
+y = Person("Sunil",54,89)
+print(y)
+
+print(Person.amount)
+
+
+class Player:
+
+    count = 0
+    def __init__(self,name,age,height):
+        self.name = name
+        self.age = age
+        self.height = height
+        Player.count += 1
+
+    def __str__(self):
+        return (f"Name:{self.name}, Age:{self.age}, Height:{self.height}")
+
+    def get_older(years):
+        self.age += years
+    
+# Inheritance
+class Stats(Player):
+
+    def __init__(self,name,age,height,position):
+        super(Stats,self).__init__(name,age,height)
+        self.position = position
+
+    def __str__(self):
+        text = super(Stats,self).__str__()
+        text += ", Position: {}".format(self.position)
+        return text
+    
+
+Player1 = Stats("Shivam",21,156,"Mid-Field")
+print(Player1)
+
+
+# Recursion
+
+def fact(n):
+    
+    if n < 1:
+        return 1
+    
+    else:
+        number = n * fact(n-1)
+        return number
+a = int(input("Enter an integer to find the factorial: "))
+print(fact(a))
+
+def fib(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        number = fib(n-1) + fib(n-2)
+        return number
+
+a = int(input("Enter a number to find the fib number: "))
+print(fib(a))
+
+
